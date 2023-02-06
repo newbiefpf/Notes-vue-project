69f673a5de91e66a588c0629f360216259df2639
newbiefpf <1326788346@qq.com>
2023-02-06 14:44:05 +0800

    共享页面开发

--- src/views/Public.vue
+++ src/views/Public.vue
@@ -1,30 +1,260 @@
 <template>
   <div>
-    <figure class="md:flex rounded-xl p-8 md:p-0">
-      <img
-        class="w-24 h-24 object-cover md:rounded-md md:w-48 md:h-auto rounded-full mx-auto"
-        src="@/assets/photo.jpeg"
-        alt=""
-        width="1000"
-        height="1411" />
-      <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
-        <div>
-          <p class="text-lg font-medium text-light-primary dark:text-dark-primary">
-            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores enim error inventore nihil, numquam optio quas suscipit vitae! Dicta
-            eligendi ipsa minus quas quod! Consectetur ducimus fugiat mollitia nostrum pariatur."
-          </p>
-        </div>
-        <div class="font-medium">
-          <div class="text-sky-blue">Demi Taylor</div>
-          <div class="text-light-primary dark:text-dark-primary">HR, Saunafolk</div>
+    <div v-if="InfoShow">
+      <div>
+        <Card class="bg-light-primary dark:bg-dark-modifier-active dark:text-dark-modifier-active cardStylHead">
+          <div style="text-align: center">
+            <h3>A high quality UI Toolkit based on Vue.js</h3>
+          </div>
+        </Card>
+      </div>
+      <div>
+        <Card class="bg-light-primary dark:bg-dark-modifier-active dark:text-dark-modifier-active cardStylTitle">
+          <div style="text-align: center">
+            <h3>A high quality UI Toolkit based on Vue.js</h3>
+          </div>
+        </Card>
+      </div>
+      <div>
+        <div
+          class="bg-light-primary dark:bg-dark-modifier-active dark:text-dark-modifier-active cardStyl"
+          v-for="item in articleList"
+          :key="item.id"
+          @click="openInfo">
+          <div class="articleGroup">
+            <div class="articleImg"></div>
+            <div class="articleContent">
+              <div class="articleTitle">{{ item.title }}</div>
+              <div class="articleDescribe"></div>
+              <div class="articleBox">
+                <div class="articleBoxItem">{{ item.details.uloadTime }}</div>
+                <div class="articleBoxItem">热度{{ item.details.visitor }}</div>
+                <div class="articleBoxItem" @click.stop="drawerOpen(item.details)">评论{{ item.details.commentList.length }}</div>
+                <div class="articleBoxItem" @click.stop="">喜欢{{ item.details.like }}</div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+      <Drawer :closable="false" v-model="drawerShow" placement="right" width="25">
+        <div :class="[theme == 'dark' ? 'bg-dark-secondary' : 'bg-light-primary']" class="drawerSty">
+          <div>
+            <h1>评论</h1>
+          </div>
+          <div v-for="item in commentList" :key="item.id">
+            <chitchat :imgUrl="item.imgUrl" :content="item.remark" :name="item.name" :dataTime="item.remarkTime">
+              <div v-for="y in item.discuss" :key="y.id" slot="childchitchat">
+                <chitchat :imgUrl="y.imgUrl" :content="y.remark" :name="y.name" :dataTime="y.remarkTime" :parent="y.parent"> </chitchat>
+              </div>
+            </chitchat>
+          </div>
+        </div>
+      </Drawer>
+    </div>
+
+    <figure v-else class="md:flex rounded-xl p-8 md:p-0">
+      <Info />
     </figure>
   </div>
 </template>
 
 <script>
+import Info from "@/components/info";
+
+import chitchat from "@/components/chitchat";
 export default {
   name: "Public",
+  data() {
+    return {
+      InfoShow: true,
+      articleList: [
+        {
+          id: 1,
+          imgUrl: "",
+          describe: "",
+          title: "袜子撒",
+          details: {
+            uloadTime: "2023-02-06",
+            visitor: 12,
+            commentList: [
+              {
+                usrId: 1,
+                name: "cs",
+                remark: "这是一条评论",
+                remarkTime: "2023-02-06",
+                imgUrl: "cs",
+                discuss: [
+                  {
+                    usrId: 2,
+                    name: "cs1",
+                    remark: "这是一条回复",
+                    remarkTime: "2023-02-06",
+                    imgUrl: "cs1",
+                    parent: {
+                      usrId: 1,
+                      name: "cs",
+                    },
+                  },
+                ],
+              },
+              {
+                usrId: 3,
+                name: "cs",
+                remark: "这是一条评论",
+                remarkTime: "2023-02-06",
+                imgUrl: "cs",
+                discuss: [
+                  {
+                    usrId: 2,
+                    name: "cs1",
+                    remark: "这是一条回复",
+                    remarkTime: "2023-02-06",
+                    imgUrl: "cs1",
+                    parent: {
+                      usrId: 3,
+                      name: "cs",
+                    },
+                  },
+                ],
+              },
+            ],
+            like: 110,
+          },
+        },
+        {
+          id: 2,
+          imgUrl: "",
+          describe: "",
+          title: "大商股份",
+          details: {
+            uloadTime: "",
+            visitor: 12,
+            commentList: [
+              {
+                usrId: 4,
+                name: "cs",
+                remark: "这是一条评论",
+                remarkTime: "2023-02-06",
+              },
+            ],
+            like: 110,
+          },
+        },
+        {
+          id: 3,
+          imgUrl: "",
+          describe: "",
+          title: "俺是个风格的",
+          details: {
+            uloadTime: "",
+            visitor: 12,
+            commentList: [
+              {
+                usrId: 5,
+                name: "cs",
+                remark: "这是一条评论",
+                remarkTime: "2023-02-06",
+              },
+            ],
+            like: 110,
+          },
+        },
+        {
+          id: 4,
+          imgUrl: "",
+          describe: "",
+          title: "鱼体一天的规范化",
+          details: {
+            uloadTime: "",
+            visitor: 12,
+            commentList: [
+              {
+                usrId: 6,
+                name: "cs",
+                remark: "这是一条评论",
+                remarkTime: "2023-02-06",
+              },
+            ],
+            like: 110,
+          },
+        },
+      ],
+      commentList: [],
+      drawerShow: false,
+    };
+  },
+  components: {
+    chitchat,
+    Info,
+  },
+  methods: {
+    openInfo() {
+      this.InfoShow = false;
+    },
+    drawerOpen(data) {
+      this.commentList = data.commentList;
+      this.drawerShow = true;
+    },
+  },
+  computed: {
+    theme() {
+      return this.$store.state.theme;
+    },
+  },
 };
 </script>
+<style lang="less">
+.cardStylHead {
+  height: 160px;
+  margin-bottom: 6px;
+}
+.cardStylTitle {
+  margin-bottom: 6px;
+  cursor: pointer;
+  height: 60px;
+}
+.cardStyl {
+  cursor: pointer;
+  margin-bottom: 8px;
+  height: 120px;
+}
+.articleGroup {
+  display: flex;
+  justify-content: flex-start;
+  .articleImg {
+    width: 25%;
+    height: 120px;
+    border: 1px red solid;
+  }
+  .articleContent {
+    width: 75%;
+    .articleTitle {
+      padding-left: 10%;
+      height: 30px;
+      line-height: 30px;
+      border: 1px red solid;
+    }
+    .articleDescribe {
+      height: 60px;
+      border: 1px red solid;
+    }
+    .articleBox {
+      display: flex;
+      align-items: center;
+      height: 30px;
+      border: 1px red solid;
+      .articleBoxItem {
+        text-align: center;
+        width: 25%;
+      }
+    }
+  }
+}
+.ivu-drawer-body {
+  padding: 0px;
+  margin: 0px;
+  .drawerSty {
+    height: 100%;
+  }
+}
+</style>
